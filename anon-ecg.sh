cf2bbb96617f3f838d9843f569a8185f1c5f7ae5
ame-ge <ame-ge@hotmail.com>
2025-04-27 05:48:48 -0700

    ame

--- anon-ecg.sh
+++ anon-ecg.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+# Function to check if we can write to a file ame
+check_write_access() {
+  local infile="$1"
+  
+  # Try writing "Hello World" to the file
+  echo "Hello World" > "$infile"
+  
+  # Check if the write was successful
+  if [ $? -eq 0 ]; then
+    echo "Write access confirmed for: $infile"
+    return 0  # Success
+  else
+    echo "Error: No write access to $infile"
+    return 1  # Failure
+  fi
+}
+
 # Function to deidentify a single XML file and write to output dir
 deidentify_file() {
   local infile="$1"
@@ -9,6 +26,13 @@ deidentify_file() {
 
   echo "De-identifying: $infile"
   echo "Saving to: $outfile"
+
+# Check write access before proceeding inserted by ame
+  check_write_access "$infile"
+  if [ $? -ne 0 ]; then
+    echo "Skipping file due to write access error: $infile"
+    return 1
+  fi
 
   xmlstarlet ed \
     -u "//*[local-name()='id' and @extension]" -v "" \
